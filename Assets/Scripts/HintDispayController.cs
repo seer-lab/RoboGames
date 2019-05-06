77107f6b8e54a49a9cff4f6efab52ca964ff0d24
Ibrahimmushtaq98 <ibrahim.mushtaq@uoit.net>
2019-05-06 15:57:41 -0400

    Added a hint controller (Does not work)
    This hint controller would work by checking if the user has used it tool incorrectly and if so issue a hint, I truely feel like I have gone wrong way on this"

---
+++ Assets/Scripts/HintDispayController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HintDispayController : MonoBehaviour
+{
+    public int projectilecode = 0;
+    public int usedprojectile = 0;
+    public int[] toolCounts = new int[stateLib.NUMBER_OF_TOOLS];
+    public int[] bonusTools = new int[stateLib.NUMBER_OF_TOOLS];
+    public GameObject[] toolLabels = new GameObject[stateLib.NUMBER_OF_TOOLS];
+    public GameObject codescreen;
+    public GameObject toolAvailableTools;
+
+    private LevelGenerator lg;
+
+    SidebarController sidebar;
+
+    // Use this for initialization
+    void Start() {
+        lg = codescreen.GetComponent<LevelGenerator>();
+        this.GetComponent<Text>().text = "";
+        sidebar = GameObject.Find("Sidebar").GetComponent<SidebarController>();
+
+        CheckAvailableTools();
+        if(projectilecode == stateLib.PROJECTILE_CODE_NO_TOOLS) {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Idea, if the user has used one of its tool, display a hint
+
+        CheckAvailableTools();
+
+        if(GlobalState.GameState == stateLib.GAMESTATE_IN_GAME && (usedprojectile < projectilecode)) {
+            Debug.Log("Show Hint here");
+        }
+    }
+    private void CheckAvailableTools() {
+        for (int i = 0; i < stateLib.NUMBER_OF_TOOLS; i++) {
+            if (toolCounts[i] + bonusTools[i] > 0) {
+                if (projectilecode == stateLib.PROJECTILE_CODE_NO_TOOLS) {
+                    projectilecode = i;
+                }
+            }
+        }
+    }
+}
