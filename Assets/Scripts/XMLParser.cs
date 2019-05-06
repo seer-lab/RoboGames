38612d949aea5e08efbe007f7e2bbffde52c296e
Ibrahimmushtaq98 <ibrahim.mushtaq@uoit.net>
2019-05-06 12:35:16 -0400

    Started working on a XML testing suite
    XMLChecker will take in a file (which is not yet implimented) and will call XMLParser to check if th file is well formed

---
+++ Assets/Scripts/XMLParser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class XMLParser : MonoBehaviour {
+    private string filename;
+    private bool isSucesses = false;
+
+    XMLParser(string filename) {
+        this.filename = filename;
+    }
+
+
+    public bool checkXML() {
+
+        isSucesses = true;
+        Debug.Log("Checking file " + filename);
+        XmlReaderSettings settings = new XmlReaderSettings();
+        return false;
+    }
+}
