746c6db1d20660391bc1a5c2aa5419aa5beb022e
Ibrahimmushtaq98 <ibrahim.mushtaq@uoit.net>
2019-05-06 15:54:17 -0400

    Updated the parser
    Parser should now be able to detect if an xml file is welformed or not

--- Assets/Scripts/XMLParser.cs
+++ Assets/Scripts/XMLParser.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Xml;
+using System.Xml.Schema;
 using UnityEngine;
+using System.IO;
 
 public class XMLParser : MonoBehaviour {
     private string filename;
@@ -9,6 +12,9 @@ public class XMLParser : MonoBehaviour {
 
     XMLParser(string filename) {
         this.filename = filename;
+        if (!File.Exists(filename)) {
+            Debug.Log("File Does not exists");
+        }
     }
 
 
@@ -17,6 +23,33 @@ public class XMLParser : MonoBehaviour {
         isSucesses = true;
         Debug.Log("Checking file " + filename);
         XmlReaderSettings settings = new XmlReaderSettings();
+        XmlReader reader = XmlReader.Create(filename, settings);
+
+        try {
+
+            XmlDocument document = new XmlDocument();
+            document.Load(reader);
+            ValidationEventHandler eventHandler = new ValidationEventHandler(ValidationEventHandler);
+            document.Validate(eventHandler);
+            return true;
+        }
+        catch (Exception ex){
+            Debug.Log("Xml not well formed");
+            Debug.Log(ex.Message);
+        }
         return false;
+    }
+
+
+    static void ValidationEventHandler(object sender, ValidationEventArgs e) {
+        switch (e.Severity) {
+            case XmlSeverityType.Error:
+                Console.WriteLine("Error: {0}", e.Message);
+                break;
+            case XmlSeverityType.Warning:
+                Console.WriteLine("Warning {0}", e.Message);
+                break;
+        }
+
     }
 }
