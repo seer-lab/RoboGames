79c57b31c8cfc3f9a525046be0c6c62d52180495
Ibrahim Mushtaq <ibrahim.mushtaq@uoit.net>
2019-06-11 10:24:54 -0400

    Auto stash before merge of "master" and "sqrlab/master"
    Basically Allows us to compile Unity project through command line

---
+++ Assets/Editor/WebGLBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class WebGLBuilder
+{
+    static void build() {
+
+    // Place all your scenes here
+    string[] scenes = {"Assets/TitleScene.unity", 
+                        "Assets/MainMenu.unity",
+                        "Assets/newgame.unity",
+                        "Assets/Cinematic.unity",
+                        "Assets/CharacterSelect.unity",
+                        "Assets/IntroScene.unity",
+                        "Assets/Transition.unity",
+                        "Assets/Credits.unity"};
+
+    string pathToDeploy = "builds/WebGLversion/";       
+
+    BuildPipeline.BuildPlayer(scenes, pathToDeploy, BuildTarget.WebGL, BuildOptions.None);      
+    }
+}
