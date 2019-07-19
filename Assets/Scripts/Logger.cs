9d803cb7ed60d44d830cb2d476cac16451e20341
Ibrahim Mushtaq <ibrahim.mushtaq@uoit.net>
2019-07-19 16:42:33 -0400

    Fixed Regex and fixed bug catcher tool

--- Assets/Scripts/Logger.cs
+++ Assets/Scripts/Logger.cs
@@ -108,11 +108,19 @@ public class Logger
         if(projectileCode == stateLib.TOOL_UNCOMMENTER){
             projectileCode = stateLib.TOOL_COMMENTER;
         }
-        Debug.Log(GlobalState.correctLine[projectileCode] + " : " + lineNumber);
-        if(checkString.IsMatch(GlobalState.correctLine[projectileCode])){
-            states.progress = "true";
-        }else{
-            states.progress = "false";
+
+        try{
+            if(checkString.IsMatch(GlobalState.correctLine[projectileCode])){
+                states.progress = "true";
+            }else{
+                states.progress = "false";
+            }
+        }catch(Exception e){
+            if(checkString.IsMatch(GlobalState.bugLine)){
+                states.progress = "true";
+            }else{
+                states.progress = "false";
+            }
         }
         states.time = time.ToString();
         states.timestamp = DateTime.Now.ToString();
