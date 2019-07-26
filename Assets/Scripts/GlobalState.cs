ad759ca4946611dfd8de9b14d24cc42a4273bd1e
Ibrahim Mushtaq <ibrahim.mushtaq@uoit.net>
2019-07-26 15:00:06 -0400

    Turn off debug mode

--- Assets/Scripts/GlobalState.cs
+++ Assets/Scripts/GlobalState.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 public static class GlobalState 
 {
     public static bool RestrictGameMode = false; 
-    public static bool DebugMode = true; 
+    public static bool DebugMode = false; 
     public static List<string> passed; 
     public static string Character {get;set;}
     public static bool IsPlaying { get; set; }
