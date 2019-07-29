d4a9d663a102424ee044ce4bd7a6344a6306ee1d
KashifIV <kashif.h77@gmail.com>
2019-07-29 14:26:49 -0400

    Text Glitches until hacked into

--- Assets/Scripts/ScoreText.cs
+++ Assets/Scripts/ScoreText.cs
@@ -12,10 +12,6 @@ public class ScoreText : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
-        text.text = stringLib.SCORE_PREFIX +  (GlobalState.CurrentLevelPoints + GlobalState.Stats.Points); 
-=======
         text.text = stringLib.SCORE_PREFIX +  (GlobalState.CurrentLevelPoints + GlobalState.totalPoints); 
->>>>>>> 9a07b32993b3c1b61feb82852987516bac4b442a
     }
 }
--- Assets/hacking.cs
+++ Assets/hacking.cs
@@ -1,1 +1,82 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacking : Obstacle
+{
+    float timeToHack = 3f; 
+    Sprite[] hackingPhases; 
+    Collider2D lastHero; 
+    BoxCollider2D hitBox; 
+    bool hacking = false; 
+    bool finishedHacking = false; 
+    bool glitching = false; 
+    public override string GetObstacleType(){
+        return "Hacking"; 
+    }
+    protected override void Initialize(){
+        string path = "Sprites/"; 
+        hackingPhases = new Sprite[]{
+            Resources.Load<Sprite>(path + "hackingOff"),
+            Resources.Load<Sprite>(path + "hacking"), 
+            Resources.Load<Sprite>(path + "hackingCOmplete")
+        };
+        hitBox = GetComponent<BoxCollider2D>(); 
+    }
+
+    void HandleResets(Collider2D hero){
+        if (lastHero != null && hitBox.IsTouching(lastHero)){
+            return; 
+        }
+        if (!finishedHacking){
+            StopAllCoroutines(); 
+            lastHero = hero; 
+            StartCoroutine(LoadHack()); 
+        }
+
+    }
+    IEnumerator GlitchText(){
+        glitching = true; 
+        TextMesh text = GameObject.Find("Code").GetComponent<TextMesh>(); 
+        text.font = Resources.Load<Font>("Fonts/HACKED"); 
+        yield return new WaitForSeconds(0.12f); 
+        text.font = Resources.Load<Font>("Fonts/CFGlitchCity-Regular"); 
+        yield return new WaitForSeconds(0.12f); 
+        text.font = Resources.Load<Font>("Fonts/HACKED"); 
+        yield return new WaitForSeconds(0.12f); 
+        text.font = Resources.Load<Font>("Fonts/Inconsolata"); 
+        yield return new WaitForSeconds(0.12f); 
+        glitching = false; 
+    }
+    IEnumerator LoadHack(){
+        hacking = true; 
+        GetComponent<SpriteRenderer>().sprite = hackingPhases[1]; 
+        yield return new WaitForSecondsRealtime(timeToHack); 
+        if (hitBox.IsTouching(lastHero)){
+            finishedHacking = true; 
+            GetComponent<SpriteRenderer>().sprite = hackingPhases[2]; 
+        }
+        else {
+            GetComponent<SpriteRenderer>().sprite = hackingPhases[0]; 
+        }
+        hacking = false; 
+        
+    }
+    protected override void UpdateProtocol(){
+        if (hacking && !finishedHacking && !hitBox.IsTouching(lastHero)){
+            StopCoroutine("LoadHack"); 
+            hacking = false; 
+            GetComponent<SpriteRenderer>().sprite = hackingPhases[0];
+        }
+        if (!glitching && !finishedHacking){
+            StartCoroutine(GlitchText()); 
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D collidingObj){
+        if (collidingObj.name == "Hero" && !finishedHacking){
+            HandleResets(collidingObj); 
+        }
+    }
+
+}
